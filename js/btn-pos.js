f2133201a1908a8fd8e40275652cced1794019dd
Kevin115 <kevinmallinson11@gmail.com>
2015-08-14 23:56:14 +0200

    refactoring js

--- js/btn-pos.js
+++ js/btn-pos.js
@@ -1,48 +1,61 @@
-var btn = document.getElementById("btn");
-var morph = document.getElementById("morph-content");
+//**********************************************************//
+//                     BASIC OPTIONS                        //
+//**********************************************************//
 
-// get browser width and height
-winW = document.documentElement.clientWidth;
-winH = document.documentElement.clientHeight;
+// Add ID name of your Button/CTA
+var btnName = "btn";
+// add your effect for morph content default option is fade
+// options: fade-in, slide-left, slide-top
+var mcEffect = "fade-in";
 
-// get height and width of btn
-var btnWidth = btn.offsetWidth;
-var btnHeight = btn.offsetHeight;
+var btn = document.getElementById(btnName);
+var morphBtn = document.getElementById("morph-btn");
+var morphContent = document.getElementById("morph-btn__content");
 
-// add css height and width to morph button
-morph.style.width = btnWidth + "px"
-morph.style.height = btnHeight + "px"
 
+// get height and width of btn and add to morph-btn
+morphBtn.style.width = btn.offsetWidth + "px";
+morphBtn.style.height = btn.offsetHeight + "px";
+
+// add effect option on page load
+morphContent.className = morphContent.className + " " + mcEffect;
+
+// if browser resizes call function onWindowResize
 window.addEventListener("resize", onWindowResize);
 
 function onWindowResize() {
-
-  // get position of btn
-  // left position
-  var left = btn.offsetLeft;
-  var percentLeft = left + "px";
-  morph.style.left = percentLeft;
-
+  // get left offset position of btn and add to morph-btn
+  morphBtn.style.left = btn.offsetLeft + "px";
 }
-  window.addEventListener("scroll", onScroll);
 
-  function onScroll() {
-    //cget top position of btn
-    var viewportOffset = btn.getBoundingClientRect();
-    var top = viewportOffset.top;
-    var pxTop = top + "px";
-    morph.style.top = pxTop;
-    console.log(pxTop)
-  }
 
-btn.addEventListener('click', function() {
-  morph.className = morph.className + " morph-content--open";
+// if scroll call function onScroll
+window.addEventListener("scroll", onScroll);
 
-  var att = ["bottom","right"];
-  for(var i = 0; i < att.length; i++){
-    morph.style[att[i]] = "0";
-  }
-  
-  btn.className = btn.className + " btn--hide";
-  event.preventDefault();
-});
+function onScroll() {
+  //get top position of btn relative to viewport
+  var viewportOffset = btn.getBoundingClientRect();
+  morphBtn.style.top = viewportOffset.top + "px";
+}
+
+
+// on btn click function
+btn.addEventListener("click", function() {
+
+  if(morphBtn.className !== "morph-btn--open" && morphContent.className !== "morph-btn__content--open") {
+      // add class to morphBtn and morphContent
+      morphBtn.className = morphBtn.className + " morph-btn--open";
+      morphContent.className = morphContent.className + " morph-btn__content--open " + mcEffect + "--open";
+      // add overflow hidden to body
+      document.body.style.overflow = "hidden";
+      // add class btn--hide to btn
+      btn.className = btn.className + " btn--hide";
+      // prevent browser to skip to top of the page on btn click
+      event.preventDefault();
+      // set bottom and right position to 0 on morphBtn
+      var att = ["bottom","right"];
+      for(var i = 0; i < att.length; i++) {
+          morphBtn.style[att[i]] = "0";
+      }
+    }
+  });
