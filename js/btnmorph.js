c0731bc49d7db194e93abfaa6163a271d8e9f561
Kevin Mallinson <Kevin@Kevins-MacBook-Pro.local>
2015-08-22 23:48:08 +0200

    just some js refactoring

--- js/btnmorph.js
+++ js/btnmorph.js
@@ -1,7 +1,6 @@
 //**********************************************************//
 //                     BASIC OPTIONS                        //
 //**********************************************************//
-
 // Add class name of your Button/CTA
 var btnName = "btn";
 // add your effect for morph content - default fades in the content
@@ -11,6 +10,7 @@ var mcEffect = "slide-left";
 // options: center, fullscreen, left-panel, right-panel
 var mcTheme = "fullscreen";
 
+
 //**********************************************************//
 //                NO NEED TO TOUCH THIS HERE                //
 //**********************************************************//
@@ -19,76 +19,78 @@ var morphBtn = document.getElementById("morph-btn");
 var morphContent = document.getElementById("morph-btn__content");
 var btnID = [];
 
+
 // get height and width of btn and add to morph-btn
 morphBtn.style.width = btn[0].offsetWidth + "px";
 morphBtn.style.height = btn[0].offsetHeight + "px";
 morphBtn.style.left = btn[0].offsetLeft + "px";
 
+
 // setAttribute ID on all btn elements
 // and push the elements to btnID array
-for(var i = 0; i < btn.length; i++){
-  btn[i].setAttribute("id", "btn" + (i+1));
-  var btnw = document.getElementById("btn" + (i+1));
-  btnID.push(btnw);
+for(var i = 0; i < btn.length; i++) {
+    btn[i].setAttribute("id", "btn" + (i+1));
+    var btnw = document.getElementById("btn" + (i+1));
+    btnID.push(btnw);
 
-  btnID[i].addEventListener("click", whatClicked(i));
+    btnID[i].addEventListener("click", whatClicked(i));
 
-  btnID[i].addEventListener("mouseover", mouseOver(i));
-
+    btnID[i].addEventListener("mouseover", mouseOver(i));
 }
+
 
 function mouseOver(i) {
-  return function(){
-    morphBtn.style.left = btnID[i].offsetLeft + "px";
-  };
+    return function() {
+        morphBtn.style.left = btnID[i].offsetLeft + "px";
+    };
 }
 
-window.addEventListener("scroll", onScroll);
 
 function onScroll() {
     var viewportOffset = btn[0].getBoundingClientRect();
     morphBtn.style.top = viewportOffset.top + "px";
 }
 
-window.addEventListener("resize", onWindowResize);
+window.addEventListener("scroll", onScroll);
+
 
 function onWindowResize() {
     morphBtn.style.left = btn[0].offsetLeft + "px";
 }
+
+window.addEventListener("resize", onWindowResize);
+
 
 // call functions on load to get position
 // of the first btn on the page
 onScroll();
 onWindowResize();
 
+
 // text effect on morph
 // if not default add effect class to morphContent
 if(mcEffect !== "default") {
-  morphContent.className += " " + mcEffect;
+    morphContent.className += " " + mcEffect;
 }
 
+
 function whatClicked(i) {
-   return function(){
-     if(morphBtn.className !== "morph-btn--open" && morphContent.className !== "morph-btn__content--open") {
-
-       // add class to morphBtn and morphContent
-       morphBtn.className += " morph-btn--open morph-btn--" + mcTheme;
-
-       // add class btn--hide to btn
-       btnID[i].style.display = "none";
-
-       // add overflow hidden to body
-       document.body.style.overflow = "hidden";
-
-       // prevent browser to skip to top of the page on btn click
-       event.preventDefault();
-
-       // check if effect for morphContent was selected or is default
-       if(mcEffect !== "default") {
-         morphContent.className += " morph-btn__content--open " + mcEffect + "--open";
-       } else {
-         morphContent.className += " morph-btn__content--open ";
-       }
-     }
-  };
+    return function() {
+        if(morphBtn.className !== "morph-btn--open" && morphContent.className !== "morph-btn__content--open") {
+            // add class to morphBtn and morphContent
+            morphBtn.className += " morph-btn--open morph-btn--" + mcTheme;
+            // add class btn--hide to btn
+            this.style.display = "none";
+            // add overflow hidden to body
+            document.body.style.overflow = "hidden";
+            // prevent browser to skip to top of the page on btn click
+            event.preventDefault();
+            // check if effect for morphContent was selected or is default
+            if(mcEffect !== "default") {
+                morphContent.className += " morph-btn__content--open " + mcEffect + "--open";
+            } else {
+                  morphContent.className += " morph-btn__content--open ";
+            }
+        }
+    };
 }
